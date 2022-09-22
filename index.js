076a3a01b6ba3b61c59eb2ac6e1e28f94c547b6a
cahyo giri <cahyogiri97@gmail.com>
2022-09-22 12:22:02 +0700

    pelanggan

--- index.js
+++ index.js
@@ -23,6 +23,43 @@ let DB = [
     }
 ];
 
+
+let DBpelanggan = [
+    {
+        id:1,
+        nomor:1,
+        jumlah:5,
+        waktu:"03.00",
+        pelayanan:"sudah"
+    },{
+        id:2,
+        nomor:3,
+        jumlah:3,
+        waktu:"04.00",
+        pelayanan:"sudah"
+    },{
+        id:3,
+        nomor:7,
+        jumlah:2,
+        waktu:"04.40",
+        pelayanan:"sudah"
+    },{
+        id:4,
+        nomor:15,
+        jumlah:5,
+        waktu:"05.30",
+        pelayanan:"belum"
+    },{
+        id:5,
+        nomor:11,
+        jumlah:7,
+        waktu:"05.35",
+        pelayanan:"belum"
+    }
+]
+
+
+
 /////////////////////////////////////////////
 /////////////////////////////////////////////
 
@@ -50,11 +87,6 @@ app.post('/menu',(req,res) => {
         harga:menuharga
     })
     res.redirect("/menu");
-});
-
-app.get('/:nama/berhasil', function(req, res){
-    const a = req.params.nama
-    res.render("berhasilnew" ,{a});
 });
 
 app.get('/menu/:id/edit', function(req, res){
@@ -94,8 +126,79 @@ app.post('/menu/:id/delete', function(req, res){
 });
 
 
+////////////////////////////////////////////////////
 
 
+app.get('/', function(req, res){
+    res.render("home");
+});
+
+app.get('/daftar', function(req, res){
+    res.render("daftar", {DBpelanggan});
+});
+
+app.get('/daftar/tambah', function(req, res){
+    res.render("tambah", {DBpelanggan});
+});
+
+app.post('/daftar', function(req, res){
+    const tambahnomor = req.body.nomor
+    const tambahjumlah = req.body.jumlah
+    const tambahwaktu = req.body.waktu
+    const tambahpelayanan = req.body.pelayanan
+    let idterbesar = DBpelanggan.reduce(
+        (prev,cur)=>prev>cur.id?prev:cur.id,0
+    )
+
+    DBpelanggan.push({
+        id:idterbesar + 1,
+        nomor:tambahnomor,
+        jumlah:tambahjumlah,
+        waktu:tambahwaktu,
+        pelayanan:tambahpelayanan
+    })
+    res.redirect("/daftar");    
+});
+
+app.get('/daftar/:id/ubah', function(req, res){
+    const ubahid = req.params.id
+    const data = DBpelanggan.filter(data => data.id == ubahid)[0]
+    res.render("ubah", {data});
+});
+
+app.post('/daftar/:id/ubah', function(req, res){
+    const idubah = Number(req.params.id)
+    const nomorubah = req.body.nomor
+    const jumlahubah = req.body.jumlah
+    const waktuubah = req.body.waktu
+    const pelayananubah = req.body.pelayanan
+    const dataubah = DBpelanggan.filter(data => data.id !== idubah)
+    DBpelanggan = [
+        ... dataubah,{
+            id:idubah,
+            nomor:nomorubah,
+            jumlah:jumlahubah,
+            waktu:waktuubah,
+            pelayanan:pelayananubah
+        }
+    ]
+    res.redirect("/daftar");
+});
+
+app.get('/daftar/:id/hapus', function(req, res){
+    const hapusid = req.params.id
+    const data = DBpelanggan.filter(data => data.id == hapusid)[0]
+    res.render("hapus", {data});
+});
+
+app.post('/daftar/:id/hapus', function(req,res){
+    const idhapus = Number(req.params.id)
+    const hapussemua = DBpelanggan.filter(data => data.id !== idhapus)
+    DBpelanggan = [
+        ... hapussemua
+    ]
+    res.redirect("/daftar");
+});
 
 
 
