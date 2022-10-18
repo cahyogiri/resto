284f4bae6a0fbdf78d2cba59842aa3ce9fb63762
cahyo giri <cahyogiri97@gmail.com>
2022-10-18 07:57:49 +0700

    daftar validation

--- route/pelanggan.js
+++ route/pelanggan.js
@@ -30,11 +30,11 @@ router.get('/', function (req, res, next) {
     connection.query('SELECT * FROM pelanggan ORDER BY id desc', function (err, rows) {
         if (err) {
             req.flash('error', err);
-            res.render('pelanggan', {
+            res.render('daftar', {
                 data: ''
             });
         } else {
-            res.render('pelanggan/index', {
+            res.render('daftar/index', {
                 data: rows 
             });
         }
@@ -44,7 +44,7 @@ router.get('/', function (req, res, next) {
 // New
 
 router.get('/new', function(req, res, ){
-    res.render("pelanggan/new"), {
+    res.render("daftar/new"), {
         nomor: '',
         jumlah: '',
         waktu: '',
@@ -92,7 +92,6 @@ router.post('/', function(req, res, next) {
                 res.render('daftar/new', {
                     nomor       : formData.nama,
                     jumlah      : formData.harga,
-                    waktu       : formData.waktu,
                     pelayanan   : formData.pelayanan                    
                 })
             } else {                
@@ -103,63 +102,117 @@ router.post('/', function(req, res, next) {
     }
 })
 
-// router.post('/', function(req, res){
-//     const tambahnomor = req.body.nomor
-//     const tambahjumlah = req.body.jumlah
-//     const tambahwaktu = req.body.waktu
-//     const tambahpelayanan = req.body.pelayanan
-//     const query = `INSERT INTO pelanggan (id, nomor, jumlah, pelayanan) VALUES (NULL, '${tambahnomor}', '${tambahjumlah}', '${tambahpelayanan}')`;
-//     connection.query(query , function (error, results, fields) {
-//         if (error) console.log(error)
-//     });
-//     res.redirect("/daftar");    
-// });
+// Edit
 
-router.get('/:id/edit', function(req, res){
-    const ubahid = req.params.id
-    connection.query(`SELECT * FROM pelanggan WHERE id='${ubahid}'` , function (error, results, fields) {
-        if (error) console.log(error)
-        const data = results[0]
-        res.render("pelanggan/edit", {data});
-    });
+router.get('/(:id)/edit', function(req, res, next){
+    let id = req.params.id;
+   
+    connection.query('SELECT * FROM pelanggan WHERE id = ' + id, function(err, rows, fields) {
+        if(err) throw err
+         
+        if (rows.length <= 0) {
+            req.flash('error', 'Data Dengan ID ' + id + " Tidak Ditemukan")
+            res.redirect('/daftar')
+        }
+        else {
+            res.render('daftar/edit', {
+                id:         rows[0].id,
+                nomor:      rows[0].nomor,
+                jumlah:     rows[0].jumlah,
+                pelayanan:  rows[0].pelayanan
+            })
+        }
+    })
 });
 
-router.post('/:id/edit', function(req, res){
-    const idubah = Number(req.params.id)
-    const nomorubah = req.body.nomor
-    const jumlahubah = req.body.jumlah
-    const waktuubah = req.body.waktu
-    const pelayananubah = req.body.pelayanan
-    connection.query(`UPDATE pelanggan SET nomor='${nomorubah}', jumlah='${jumlahubah}', pelayanan='${pelayananubah}' WHERE id='${idubah}'` , function (error, results, fields) {
-        if (error) console.log(error)
-        console.log(results)
-    });
-    res.redirect("/daftar");
+router.post('/:id/edit', function(req, res, next){
+    const id        = req.params.id;
+    const nomor     = req.body.nomor;
+    const jumlah    = req.body.jumlah;
+    const waktu     = req.body.waktu;
+    const pelayanan = req.body.pelayanan;
+    let errors  = false;
+
+    if(nomor.length === 0) {
+        errors = true;
+        req.flash('error_nomor', "Silahkan Isi Nomor Meja")
+    }
+
+    if(jumlah.length === 0) {
+        errors = true;
+        req.flash('error_jumlah', "Silahkan Isi Jumlah Pelanggan")
+    }
+
+    if(errors) {
+        res.render('daftar/edit', {
+            id: req.params.id,
+            nomor    : nomor,
+            jumlah   : jumlah,
+            pelayanan: pelayanan
+        })
+    }
+
+    if( !errors ) {   
+ 
+        let formData = {
+            nomor    : nomor,
+            jumlah   : jumlah,
+            pelayanan: pelayanan
+        }
+
+        connection.query('UPDATE pelanggan SET ? WHERE id = ' + id, formData, function(err, result) {
+            if (err) {
+                req.flash('error', err)
+                res.render('daftar/edit', {
+                    id:     req.params.id,
+                    nomor       : formData.nama,
+                    jumlah      : formData.harga,
+                    pelayanan   : formData.pelayanan
+                })
+            } else {
+                req.flash('success', 'Data Berhasil Diubah!');
+                res.redirect('/daftar');
+            }
+        })
+    }
 });
 
-router.get('/:id/delete', function(req, res){
-    const hapusid = req.params.id
-    connection.query(`SELECT * FROM pelanggan WHERE id='${hapusid}'` , function (error, results, fields) {
-        if (error) console.log(error)
-        const data = results[0]
-        res.render("pelanggan/delete", {data});
-    });
-});   
+// Delete
 
-router.post('/:id/delete', function(req,res){
-    const idhapus = Number(req.params.id)
-    connection.query(`DELETE FROM pelanggan WHERE id='${idhapus}'` , function (error, results, fields) {
-        if (error) console.log(error)
-        console.log(results)
-    });
-    res.redirect("/daftar");
+router.get('/(:id)/delete', function(req, res, next){
+    let id = req.params.id;
+   
+    connection.query('SELECT * FROM pelanggan WHERE id = ' + id, function(err, rows, fields) {
+        if(err) throw err
+         
+        if (rows.length <= 0) {
+            req.flash('error', 'Data Dengan ID ' + id + " Tidak Ditemukan")
+            res.redirect('/daftar')
+        }
+        else {
+            res.render('daftar/delete', {
+                id:         rows[0].id,
+                nomor:      rows[0].nomor,
+                jumlah:     rows[0].jumlah,
+                pelayanan:  rows[0].pelayanan
+            })
+        }
+    })
 });
 
-
-
-
-
-
+router.post('/:id/delete', function(req, res, next) {
+    let id = req.params.id;
+     
+    connection.query('DELETE FROM pelanggan WHERE id = ' + id, function(err, result) {
+        if (err) {
+            req.flash('error', err)
+            res.redirect('/daftar')
+        } else {
+            req.flash('success', 'Data Berhasil Dihapus!')
+            res.redirect('/daftar')
+        }
+    })
+})
 
 
 module.exports = router;
